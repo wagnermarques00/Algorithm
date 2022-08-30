d8d6fa2c91ca3cbdc22b0e4e88ba6c85fd3606a1
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-08-30 18:01:13 -0300

    LinearSearch

---
+++ Java/Algorithm/src/main/java/Algorithm/Algorithms/LinearSearch.java
@@ -0,0 +1,42 @@
+package Algorithm.Algorithms;
+
+import Algorithm.Objects.Score;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Score[] scores = {
+                new Score("Alfred", 10.0),
+                new Score("Amber", 7.0),
+                new Score("Christian", 6.7),
+                new Score("Gerry", 5.0),
+                new Score("Joy", 4.0),
+                new Score("Morris", 3.0),
+                new Score("Sydney", 9.0),
+                new Score("Vera", 8.5),
+                new Score("Veronica", 9.3),
+        };
+
+        double numberSearched = 9.3;
+        int numberFound = linearSearch(scores, 0, scores.length, numberSearched);
+
+        printResult(scores, numberSearched, numberFound);
+    }
+
+    private static int linearSearch(Score[] scores, int from, int to, double searching) {
+        for(int actual = from; actual < to; actual++) {
+            if(scores[actual].getScore() == searching) {
+                return actual;
+            }
+        }
+        return -1;
+    }
+
+    private static void printResult(Score[] scores, double searched, int positionFound) {
+        System.out.println("The number " + searched + " was found in the position: " + positionFound);
+        for(int i = 0; i < scores.length; i++) {
+            Score score = scores[i];
+
+            System.out.println(score.getStudentName() + ": " + score.getScore());
+        }
+    }
+}
---
+++ Portugol/BuscaLinear.por
@@ -0,0 +1,41 @@
+programa {
+	
+	inclua biblioteca Util --> u
+	real valores[] = {10.0, 7.0, 6.7, 5.0, 4.0, 3.0, 9.0, 8.5, 9.3 }
+	real valorProcurar = 9.3
+	
+	funcao inicio() {
+		inteiro numeroEncontrado = buscaLinear(0, u.numero_elementos(valores), valorProcurar)
+		imprimeResultado(valorProcurar, numeroEncontrado)
+	}
+
+	funcao inteiro buscaLinear(inteiro de, inteiro paraO, real procurando) {
+		para(inteiro atual = de; atual < paraO; atual++) {
+			se(valores[atual] == procurando) {
+				retorne atual
+			}
+		}
+		retorne -1
+	}
+
+	funcao imprimeResultado(real encontrado, inteiro posicaoEncontrada) {
+		escreva("O número ", encontrado, " foi encontrado na posição ", posicaoEncontrada, 
+			   ". O vetor completo encontra-se abaixo:\n")
+		para(inteiro atual = 0; atual < u.numero_elementos(valores); atual++) {
+			real valor = valores[atual]
+
+			escreva("Posição ", atual, ": ", valor, "\n")
+		}
+	}
+}
+/* $$$ Portugol Studio $$$ 
+ * 
+ * Esta seção do arquivo guarda informações do Portugol Studio.
+ * Você pode apagá-la se estiver utilizando outro editor.
+ * 
+ * @POSICAO-CURSOR = 875; 
+ * @PONTOS-DE-PARADA = ;
+ * @SIMBOLOS-INSPECIONADOS = ;
+ * @FILTRO-ARVORE-TIPOS-DE-DADO = inteiro, real, logico, cadeia, caracter, vazio;
+ * @FILTRO-ARVORE-TIPOS-DE-SIMBOLO = variavel, vetor, matriz, funcao;
+ */
